32523c5e0ef5c38f5b630903c5a4bbdab41b92bb
chenwenzhe <415711543@qq.com>
2019-12-11 17:18:49 +0800

    添加 styled component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import router from '../router';
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
 
+const tabs = [
+  {
+    link: '/bracket',
+    text: '赛前',
+  },
+  {
+    link: '/immediate',
+    text: '赛中',
+  },
+  {
+    link: '/analysis',
+    text: '赛后',
+  },
+];
 const Layout = () => (
   <div>
     <ul>
-      <li>
-        <Link to="/bracket">赛前</Link>
-      </li>
-      <li>
-        <Link to="/immediate">赛中</Link>
-      </li>
-      <li>
-        <Link to="/analysis">赛后</Link>
-      </li>
+      {tabs.map(item => (
+        <li>
+          <Link to={item.link}>{item.text}</Link>
+        </li>
+      ))}
     </ul>
     <hr />
     <Switch>
